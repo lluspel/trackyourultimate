cb4c4f0606658fc924b7420f8513b669cd778e82
rodmarZZ <ismael.rodriguez@live.u-tad.com>
2019-12-27 18:30:38 +0100

    day 1

---
+++ app/src/main/java/com/example/trackyourultimate/PlayersActivity.java
@@ -0,0 +1,14 @@
+package com.example.trackyourultimate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_players);
+    }
+}
